6acf5ef62257aa1ef49c89934f91d0e8e11145b1
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2015-03-21 08:23:05 +0300

    [dicos] '/dicos/delete': OK

--- public/scripts/controllers/dico/dicosController.js
+++ public/scripts/controllers/dico/dicosController.js
@@ -3,12 +3,12 @@
     $scope.dico = {};
     $scope.dicos = [];
     $scope.filter = {};
-
+    
     $http.get('/cultures/getAll').success(function (data) {
         $scope.cultures = data;
     }).then(function () {
         $http.get('/dicos/getAll').success(function (data) {
-            data.sort(function(a, b) {
+            data.sort(function (a, b) {
                 if (a.Text < b.Text) return -1;
                 if (a.Text > b.Text) return 1;
                 return 0;
@@ -20,11 +20,11 @@
             $scope.dico.To = $scope.cultures.filter(function (d) { return d._id == $scope.dico.ToId; })[0].Description;
             $scope.illustrations = $scope.dico.Illustrations;
             $scope.tableIllustration.reload();
-
+            
             $scope.tableDico.reload();
         });
     });
-
+    
     $scope.tableDico = new ngTableParams({
         page: 1,
         count: 10
@@ -36,7 +36,7 @@
         $scope: { dicos: {} }
     });
     $scope.tableDico.settings().$scope = $scope;
-
+    
     $scope.changeLabelSelected = function (model) {
         angular.forEach($scope.dicos, function (d) {
             d.$selected = false;
@@ -48,7 +48,7 @@
         $scope.illustrations = $scope.dico.Illustrations;
         $scope.tableIllustration.reload();
     }
-
+    
     $scope.illustrations = [];
     $scope.tableIllustration = new ngTableParams({
         page: 1,
@@ -61,18 +61,18 @@
         $scope: { illustrations: {} }
     });
     $scope.tableDico.settings().$scope = $scope;
-
+    
     $scope.remove = function (id) {
         var response = confirm("Are you sure you want to delete this dico?");
         if (response) {
-            $http.get(apiUrl + 'Dico/Delete/' + id).success(function () {
+            $http.get('/dicos/delete/' + id).success(function () {
             }).success(function () {
-                $scope.dicos = $scope.dicos.filter(function (d) { return d.Id != id; });
+                $scope.dicos = $scope.dicos.filter(function (d) { return d._id != id; });
                 $scope.tableDico.reload();
             });
         }
     }
-
+    
     $scope.search = function () {
         var url = '/dicos/search/' + $scope.filter.Text;
         if ($scope.filter.Text == undefined)
@@ -81,24 +81,24 @@
             $scope.dicos = response;
             if (response.length == 0)
                 $scope.dicos = [];
-
+            
             $scope.dico = $scope.dicos.length > 0 ? $scope.dicos[0] : {};
             $scope.dico.$selected = true;
-
+            
             var from = $scope.cultures.filter(function (d) { return d.Id == $scope.dico.FromId; });
             if (from.length > 0)
                 $scope.dico.From = from[0].Description;
-
+            
             var to = $scope.cultures.filter(function (d) { return d.Id == $scope.dico.ToId; });
             if (to.length > 0)
                 $scope.dico.To = to[0].Description;
-
+            
             $scope.illustrations = $scope.dico.Illustrations;
-            if (response.length == 0) 
+            if (response.length == 0)
                 $scope.illustrations = [];
             
             $scope.tableIllustration.reload();
-
+            
             $scope.tableDico.reload();
         });
     }
