ac49e749628841cd7863f064a3fb610b89da41b7
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2015-03-22 20:15:21 +0300

    use underscore in notes.js logic

--- logic/notes.js
+++ logic/notes.js
@@ -1,6 +1,6 @@
 ﻿var ObjectId = require('mongodb').ObjectID;
+var _ = require('underscore');
 var Repository = require('../model/repositoryv2.js');
-require('../helpers/filterHelper.js');
 
 exports.getNotesByVerseId = function (req, res) {
     var notesRepository = new Repository('notes');
@@ -12,10 +12,10 @@ exports.getNotesByVerseId = function (req, res) {
     }).then(function (tags) {
         for (var i = 0; i < notes.length; i++) {
             for (var j = 0; j < notes[i].TagIdList.length; j++) {
-                notes[i].TagIdList[j] = tags.filter(function (tag) { return tag._id == notes[i].TagIdList[j]; })[0].Description;
+                notes[i].TagIdList[j] = _.find(tags, function (tag) { return tag._id == notes[i].TagIdList[j]; }).Description;
             }
         }
-        var myNote = notes.filter(function (note) { return note.CreatedBy == req.params.author; })[0];
+        var myNote = _.find(notes, function (note) { return note.CreatedBy == req.params.author; });
         var otherNotes = notes.filter(function (note) { return note.CreatedBy != req.params.author; });
         var results = {
             MyNote: myNote,
