b801ef1442b42d40d782a42b4ce1da13644ad753
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2015-03-22 11:07:37 +0300

    Add filterHelper

---
+++ helpers/filterHelper.js
@@ -0,0 +1,24 @@
+﻿var ObjectId = require('mongodb').ObjectID;
+
+Object.prototype.toAnyFilter = function () {
+    var filters = this;
+    for (var filter in filters) {
+        if (filter.indexOf('Id') > 0 && filter != 'VerseId') {
+            filters[filter] = new ObjectId(filters[filter]);
+        }
+        if (filters[filter] instanceof Object) {
+            var subFilters = filters[filter];
+            for (var subFilter in subFilters) {
+                if (subFilter == 'lte') {
+                    subFilters.$lte = subFilters[subFilter];
+                    delete subFilters.lte;
+                }
+                if (subFilter == 'gte') {
+                    subFilters.$gte = subFilters[subFilter];
+                    delete subFilters.gte;
+                }
+            }
+        }
+    }
+    return filters;
+}
