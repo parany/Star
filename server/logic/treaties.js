bb189da5cafbb63b06b9fe99ccff514eb38d1deb
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2015-04-18 20:49:21 +0300

    [TREATY] Fix issue in Treaty logic.

--- server/logic/treaties.js
+++ server/logic/treaties.js
@@ -8,61 +8,78 @@ require('../helpers/numberHelper.js');
 var treatiesRepository = new Repository('treaties');
 var tagsRepository = new Repository('tags');
 
-exports.getByDate = function (req, res) {
+exports.getByDate = function(req, res) {
     var date = new Date(req.params.date);
     var dateTime = date.getTime();
     var treatiesDocs = [];
-    treatiesRepository.find({ CreatedBy: req.params.author, sort: { Date: 1 } }).then(function (docs) {
+    treatiesRepository.find({
+        CreatedBy: req.params.author,
+        sort: {
+            Date: 1
+        }
+    }).then(function(docs) {
         treatiesDocs = docs;
-        return tagsRepository.find({ Type: 'Treaty' });
-    }).then(function (tagDocs) {
+        return tagsRepository.find({
+            Type: 'Treaty'
+        });
+    }).then(function(tagDocs) {
         var firstMsOfDay = date.getFirstMsOfDay();
         var lastMsOfDay = date.getLastMsOfDay();
-        var explications = treatiesDocs.filter(function (a) {
+        var treaties = treatiesDocs.filter(function(a) {
             return a.Date >= firstMsOfDay && a.Date <= lastMsOfDay;
         });
-        var dates = treatiesDocs.map(function (a) { return a.Date; }).filter(function (d) {
+        var dates = treatiesDocs.map(function(a) {
+            return a.Date;
+        }).filter(function(d) {
             return d < firstMsOfDay || d > lastMsOfDay;
         });
-        var prevs = dates.filter(function (d) {
+        var prevs = dates.filter(function(d) {
             return d < dateTime;
-        }).sort(function (d1, d2) {
+        }).sort(function(d1, d2) {
             return d2 - d1;
         });
-        var nexts = dates.filter(function (d) {
+        var nexts = dates.filter(function(d) {
             return d > dateTime;
-        }).sort(function (d1, d2) {
+        }).sort(function(d1, d2) {
             return d1 - d2;
         });
-        for (var i = 0; i < explications.length; i++) {
-            for (var j = 0; j < explications[i].TagIdList.length; j++) {
-                explications[i].TagIdList[j] = _.find(tagDocs, function (t) {
-                    return t._id.equals(new ObjectId(explications[i].TagIdList[j]));
+        for (var i = 0; i < treaties.length; i++) {
+            for (var j = 0; j < treaties[i].TagIdList.length; j++) {
+                treaties[i].TagIdList[j] = _.find(tagDocs, function(t) {
+                    return t._id.equals(new ObjectId(treaties[i].TagIdList[j]));
                 }).Description;
             }
         }
         var results = {
             Prev: prevs.length == 0 ? null : new Date(prevs[0]).toAnyString(),
             Next: nexts.length == 0 ? null : new Date(nexts[0]).toAnyString(),
-            Treaties: explications
+            Treaties: treaties
         };
         res.send(results);
     });
 }
 
-exports.search = function (req, res) {
+exports.search = function(req, res) {
     var treatiesDocs = [];
     treatiesRepository.find({
-        $or: [
-            { Title: { $regex: req.params.text } }, 
-            { Content: { $regex: req.params.text } }]
-    }).then(function (docs) {
+        $or: [{
+            Title: {
+                $regex: req.params.text
+            }
+        }, {
+            Content: {
+                $regex: req.params.text
+            }
+        }]
+    }).then(function(docs) {
         treatiesDocs = docs;
-        return tagsRepository.find({ Type: 'Treaty' });
-    }).then(function (tagDocs) {
+        return tagsRepository.find({
+            Type: 'Treaty'
+        });
+    }).then(function(tagDocs) {
         for (var i = 0; i < treatiesDocs.length; i++) {
             for (var j = 0; j < treatiesDocs[i].TagIdList.length; j++) {
-                treatiesDocs[i].TagIdList[j] = _.find(tagDocs, function (t) {
+                treatiesDocs[i].TagIdList[j] = _.find(tagDocs, function(t) {
                     return t._id.equals(new ObjectId(treatiesDocs[i].TagIdList[j]));
                 }).Description;
             }
