74e1c2956e308d1258b80e9cc772bf17b7b9af5d
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2015-05-14 16:41:13 +0300

    [FUNCTIONAL PROGRAMMING] * Dans la méthode "getArticlesInTheSameDate" de genericLogic, utiliser "without" au lieu de ce weird sort. => pas besoin d'utiliser "without", pas même besoin d'utiliser "sort".

--- server/logic/genericLogic.js
+++ server/logic/genericLogic.js
@@ -4,50 +4,48 @@ var _ = require('underscore');
 var Repository = require('../model/repository.js');
 require('../helpers/filterHelper.js');
 
-exports.findAll = function(req, res) {
+exports.findAll = function (req, res) {
     var repository = new Repository(req.params.collectionName);
-    repository.find({}).then(function(docs) {
+    repository.find({}).then(function (docs) {
         res.send(docs);
     });
 };
 
-exports.find = function(req, res) {
+exports.find = function (req, res) {
     var repository = new Repository(req.params.collectionName);
     var filters = req.body.toAnyFilter();
-    repository.find(filters).then(function(docs) {
+    repository.find(filters).then(function (docs) {
         res.send(docs);
     });
 };
 
-exports.findOne = function(req, res) {
+exports.findOne = function (req, res) {
     var repository = new Repository(req.params.collectionName);
-    repository.findOne({
-        _id: new ObjectId(req.params.id)
-    }).then(function(docs) {
+    repository.findOne({ _id: new ObjectId(req.params.id) }).then(function (docs) {
         res.send(docs);
     });
 };
 
-exports.update = function(req, res) {
+exports.update = function (req, res) {
     var repository = new Repository(req.params.collectionName);
     var obj = req.body;
     obj._id = new ObjectId(obj._id);
     obj.UpdatedOn = new Date().getTime();
-    repository.save(obj).then(function(ret) {
+    repository.save(obj).then(function (ret) {
         res.json(ret);
     });
 };
 
-exports.insert = function(req, res) {
+exports.insert = function (req, res) {
     var repository = new Repository(req.params.collectionName);
     var obj = req.body;
     obj.CreatedOn = new Date().getTime();
-    repository.insert(obj).then(function(ret) {
+    repository.insert(obj).then(function (ret) {
         res.json(ret);
     });
 };
 
-exports.getByDate = function(req, res) {
+exports.getByDate = function (req, res) {
     var date = new Date(req.params.date);
     var firstMsOfDay = date.getFirstMsOfDay();
     var lastMsOfDay = date.getLastMsOfDay();
@@ -55,47 +53,43 @@ exports.getByDate = function(req, res) {
     repository.find({
         CreatedBy: req.params.author,
         Date: { $gte: firstMsOfDay, $lte: lastMsOfDay }
-    }).then(function(docs) {
+    }).then(function (docs) {
         res.send(docs);
     });
 };
 
-exports.getActivities = function(req, res) {
+exports.getActivities = function (req, res) {
     var repository = new Repository(req.params.collectionName);
     var userActionRepository = new Repository('userActions');
     var operations = [];
     userActionRepository.find({
         collection: req.params.collectionName,
         createdBy: req.params.author,
-        sort: {
-            date: -1
-        },
+        sort: { date: -1 },
         limit: 6
-    }).then(function(docs) {
+    }).then(function (docs) {
         operations = docs;
         return repository.find({
             CreatedBy: req.params.author,
-            sort: {
-                Date: 1
-            }
+            sort: { Date: 1 }
         });
-    }).then(function(docs) {
+    }).then(function (docs) {
         var date = new Date();
         var firstTimeOfYear = date.getFirstMsOfYear();
         var lastTimeOfYear = date.getLastMsOfYear();
-        var yearItems = docs.filter(function(a) {
+        var yearItems = docs.filter(function (a) {
             return a.Date >= firstTimeOfYear && a.Date <= lastTimeOfYear;
         });
         date = new Date();
         var firstTimeOfMonth = date.getFirstMsOfMonth();
         var lastTimeOfMonth = date.getLastMsOfMonth();
-        var monthItems = docs.filter(function(a) {
+        var monthItems = docs.filter(function (a) {
             return a.Date >= firstTimeOfMonth && a.Date <= lastTimeOfMonth;
         });
         date = new Date();
         var firstMsOfWeek = date.getFirstMsOfWeek();
         var lastMsOfWeek = date.getLastMsOfWeek();
-        var weekItems = docs.filter(function(a) {
+        var weekItems = docs.filter(function (a) {
             return a.Date >= firstMsOfWeek && a.Date <= lastMsOfWeek;
         });
         var item = {
@@ -109,10 +103,10 @@ exports.getActivities = function(req, res) {
     });
 };
 
-exports.search = function(req, res) {
+exports.search = function (req, res) {
     var repository = new Repository(req.params.collectionName);
     var columns = req.body.filters;
-    var filters = columns.map(function(column) {
+    var filters = columns.map(function (column) {
         var filter = {};
         filter[column] = {
             $regex: req.params.text
@@ -122,37 +116,31 @@ exports.search = function(req, res) {
     repository.find({
         $or: filters,
         projection: req.body.projection || {}
-    }).then(function(agendas) {
+    }).then(function (agendas) {
         res.send(agendas);
     });
 };
 
-exports.delete = function(req, res) {
+exports.delete = function (req, res) {
     var repository = new Repository(req.params.collectionName);
-    repository.delete(new ObjectId(req.params.id)).then(function(ret) {
+    repository.delete(new ObjectId(req.params.id)).then(function (ret) {
         res.json(ret);
     });
 };
 
-exports.getArticlesInTheSameDate = function(req, res) {
+exports.getArticlesInTheSameDate = function (req, res) {
     var articles = ['treaties', 'agendas', 'explications', 'news'];
-    articles = articles.sort(function(art) {
-        return req.params.collectionName !== art;
-    });
     var date = new Date(parseInt(req.params.date));
     var tasks = [];
-    articles.forEach(function(article) {
+    articles.forEach(function (article) {
         var repository = new Repository(article);
         var task = repository.find({
-            Date: {
-                $gte: date.getFirstMsOfDay(),
-                $lte: date.getLastMsOfDay()
-            }
+            Date: { $gte: date.getFirstMsOfDay(), $lte: date.getLastMsOfDay() }
         });
         tasks.push(task);
     });
     var results = {};
-    Q.all(tasks).spread(function(list0, list1, list2, list3) {
+    Q.all(tasks).spread(function (list0, list1, list2, list3) {
         results[articles[0]] = list0;
         results[articles[1]] = list1;
         results[articles[2]] = list2;
@@ -161,46 +149,32 @@ exports.getArticlesInTheSameDate = function(req, res) {
     });
 };
 
-exports.getPrevNearArticles = function(req, res) {
+exports.getPrevNearArticles = function (req, res) {
     var date = new Date(parseInt(req.params.date));
     console.log(date);
     var dateTime = date.getTime();
     var repository = new Repository(req.params.collectionName);
     repository.find({
-        Date: {
-            $lt: dateTime
-        },
-        sort: {
-            Date: -1
-        },
-        projection: {
-            Date: 1,
-            Title: 1
-        },
+        Date: { $lt: dateTime },
+        sort: { Date: -1 },
+        projection: { Date: 1, Title: 1 },
         limit: 5
-    }).then(function(docs) {
+    }).then(function (docs) {
         res.send(docs);
     });
 };
 
-exports.getNextNearArticles = function(req, res) {
+exports.getNextNearArticles = function (req, res) {
     var date = new Date(parseInt(req.params.date));
     console.log(date);
     var dateTime = date.getTime();
     var repository = new Repository(req.params.collectionName);
     repository.find({
-        Date: {
-            $gt: dateTime
-        },
-        sort: {
-            Date: 1
-        },
-        projection: {
-            Date: 1,
-            Title: 1
-        },
+        Date: { $gt: dateTime },
+        sort: { Date: 1 },
+        projection: { Date: 1, Title: 1 },
         limit: 5
-    }).then(function(docs) {
+    }).then(function (docs) {
         res.send(docs);
     });
 };
