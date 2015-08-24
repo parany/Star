b7d87262ec514a15d9c07ab0d9ba0ec9272bd542
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2015-08-24 08:28:48 +0300

    [node.js] Memory leak - Apparemment, il y a rÃ©ellement du memory leak. Je soupconne le repository pour cela. Peut Ãªtre qu'on doit le modifier et n'utiliser qu'une seule instance partout, ie Ã©viter le: var agendaRepository = new Repository('agendas'); var agendas = agendaRepository.findAll();
    mais plutÃ´t:
    var agendas = repository.findAll('agendas', {});
    
    - Ou bien dÃ©tecter tous les memory leaks via l'utilisation des modules pour cela.

--- server/logic/treatyLogic.js
+++ server/logic/treatyLogic.js
@@ -1,51 +1,55 @@
 ﻿var _ = require('underscore');
 
-var Repository = require('../model/repository.js');
+var repository = require('../model/repository.js');
 require('../helpers/dateHelper.js');
 require('../helpers/numberHelper.js');
 
-var treatiesRepository = new Repository('treaties');
-var tagsRepository = new Repository('tags');
-
-exports.getByDate = function (req, res) {
+exports.getByDate = function(req, res) {
     var date = new Date(req.params.date);
     var treaties = [];
     var firstMsOfDay = date.getFirstMsOfDay();
     var lastMsOfDay = date.getLastMsOfDay();
-    treatiesRepository.find({
+    repository.find('treaties', {
         CreatedBy: req.params.author,
-        sort: { Date: 1 },
-        Date: { $gte: firstMsOfDay, $lte: lastMsOfDay }
-    }).then(function (docs) {
+        sort: {
+            Date: 1
+        },
+        Date: {
+            $gte: firstMsOfDay,
+            $lte: lastMsOfDay
+        }
+    }).then(function(docs) {
         treaties = docs;
-        return tagsRepository.find({
+        return repository.find('tags', {
             Type: 'Treaty'
         });
-    }).then(function (tagDocs) {
-        tagDocs.forEach(function (tag) {
+    }).then(function(tagDocs) {
+        tagDocs.forEach(function(tag) {
             tag._id = tag._id.toString();
         });
         for (var i = 0; i < treaties.length; i++) {
             for (var j = 0; j < treaties[i].TagIdList.length; j++) {
-                treaties[i].TagIdList[j] = _.findWhere(tagDocs, { _id: treaties[i].TagIdList[j].toString() }).Description;
+                treaties[i].TagIdList[j] = _.findWhere(tagDocs, {
+                    _id: treaties[i].TagIdList[j].toString()
+                }).Description;
             }
         }
         res.send(treaties);
     });
 };
 
-exports.search = function (req, res) {
-    treatiesRepository.find({
+exports.search = function(req, res) {
+    repository.find('treaties', {
         $or: [{
             Title: {
                 $regex: req.params.text
             }
         }, {
-                Content: {
-                    $regex: req.params.text
-                }
-            }]
-    }).then(function (docs) {
+            Content: {
+                $regex: req.params.text
+            }
+        }]
+    }).then(function(docs) {
         res.send(docs);
     });
 };
--- server/model/repository.js
+++ server/model/repository.js
@@ -3,103 +3,98 @@ var mongodb = require('mongodb');
 var Q = require('Q');
 var dbConfig = require('../config/db.json');
 
-module.exports = Repository;
+var db = new mongodb.Db(
+	dbConfig.name,
+	new mongodb.Server(dbConfig.host, dbConfig.port, {
+		auto_reconnect: true,
+		poolSize: 20
+	}), {
+		w: 1
+	}
+);
 
-function Repository(collectionName) {
-    var db = new mongodb.Db(
-        dbConfig.name,
-        new mongodb.Server(dbConfig.host, dbConfig.port, {
-            auto_reconnect: true,
-            poolSize: 20
-        }), {
-            w: 1
-        }
-    );
-    this.collection = db.collection(collectionName);
-}
+exports.find = function(collectionName, filters) {
+	var deferred = Q.defer();
+	var sort = filters.sort || {};
+	var limit = filters.limit || dbConfig.limit;
+	var projection = filters.projection || {};
 
-Repository.prototype.find = function(filters) {
-    var deferred = Q.defer();
-    var sort = filters.sort || {};
-    var limit = filters.limit || dbConfig.limit;
-    var projection = filters.projection || {};
+	delete filters.sort;
+	delete filters.limit;
+	delete filters.projection;
 
-    delete filters.sort;
-    delete filters.limit;
-    delete filters.projection;
-
-    this.collection
-        .find(filters, projection)
-        .sort(sort)
-        .limit(limit)
-        .toArray(function(err, doc) {
-            if (err) return deferred.reject(err);
-            return deferred.resolve(doc);
-        });
-    return deferred.promise;
+	db.collection(collectionName)
+		.find(filters, projection)
+		.sort(sort)
+		.limit(limit)
+		.toArray(function(err, doc) {
+			if (err) return deferred.reject(err);
+			return deferred.resolve(doc);
+		});
+	return deferred.promise;
 };
 
-Repository.prototype.findOne = function(filters) {
-    var deferred = Q.defer();
-    this.collection.findOne(filters, function(err, doc) {
-        if (err) return deferred.reject(err);
-        return deferred.resolve(doc);
-    });
-    return deferred.promise;
+exports.findOne = function(collectionName, filters) {
+	var deferred = Q.defer();
+	db.collection(collectionName).findOne(filters, function(err, doc) {
+		if (err) return deferred.reject(err);
+		return deferred.resolve(doc);
+	});
+	return deferred.promise;
 };
 
-Repository.prototype.insert = function(obj) {
-    var deferred = Q.defer();
-    this.collection.insert(obj, function(err, ret) {
-        if (err) return deferred.reject(err);
-        return deferred.resolve(ret);
-    });
-    return deferred.promise;
+exports.insert = function(collectionName, obj) {
+	var deferred = Q.defer();
+	db.collection(collectionName).insert(obj, function(err, ret) {
+		if (err) return deferred.reject(err);
+		return deferred.resolve(ret);
+	});
+	return deferred.promise;
 };
 
-Repository.prototype.save = function(obj) {
-    var deferred = Q.defer();
-    obj.UpdatedOn = new Date().getTime();
-    var id = new ObjectId(obj._id);
-    delete obj._id;
-    this.collection.update({
-        _id: id
-    }, {
-        $set: obj
-    }, function(err, doc) {
-        if (err) return deferred.reject(err);
-        return deferred.resolve(doc);
-    });
-    return deferred.promise;
+exports.save = function(collectionName, obj) {
+	var deferred = Q.defer();
+	obj.UpdatedOn = new Date().getTime();
+	var id = new ObjectId(obj._id);
+	delete obj._id;
+	db.collection(collectionName).update({
+		_id: id
+	}, {
+		$set: obj
+	}, function(err, doc) {
+		if (err) return deferred.reject(err);
+		return deferred.resolve(doc);
+	});
+	return deferred.promise;
 };
 
-Repository.prototype.delete = function(id) {
-    var deferred = Q.defer();
-    this.collection.remove({
-        _id: id
-    }, {
-        atomic: true
-    }, function(err, doc) {
-        if (err) return deferred.reject(err);
-        return deferred.resolve(doc);
-    });
-    return deferred.promise;
+exports.delete = function(collectionName, id) {
+	var deferred = Q.defer();
+	db.collection(collectionName).remove({
+		_id: id
+	}, {
+		atomic: true
+	}, function(err, doc) {
+		if (err) return deferred.reject(err);
+		return deferred.resolve(doc);
+	});
+	return deferred.promise;
 };
 
-Repository.prototype.group = function(key, condition, reduce, initial) {
-    var deferred = Q.defer();
-    this.collection.group(key, condition, initial, reduce, {}, {}, function(err, doc) {
-        if (err) return deferred.reject(err);
-        return deferred.resolve(doc);
-    });
-    return deferred.promise;
+exports.group = function(collectionName, key, condition, reduce, initial) {
+	var deferred = Q.defer();
+	db.collection(collectionName).group(key, condition, initial, reduce, {}, {}, function(err, doc) {
+		if (err) return deferred.reject(err);
+		return deferred.resolve(doc);
+	});
+	return deferred.promise;
 };
 
-Repository.prototype.count = function(filter) {
-    var deferred = Q.defer();
-    this.collection.count(filter, function(err, nb) {
-        if (err) return deferred.reject(err);
-        return deferred.resolve(nb);
-    });
-    return deferred.promise;
+exports.count = function(collectionName, filter) {
+	var deferred = Q.defer();
+	db.collection(collectionName).count(filter, function(err, nb) {
+		if (err) return deferred.reject(err);
+		return deferred.resolve(nb);
+	});
+	return deferred.promise;
 };
