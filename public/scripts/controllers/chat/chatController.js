960169ac70f39f2c0552342f36577218a3e58da0
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2015-10-08 14:39:47 +0300

    [chat] works now

--- public/scripts/controllers/chat/chatController.js
+++ public/scripts/controllers/chat/chatController.js
@@ -1,8 +1,11 @@
 starApp.controller('chatController', function($scope, starTable, accountService) {
 	$scope.page.title = 'Chat';
 
+	$scope.messages = {};
+	$scope.selectedMessages = [];
 	$scope.users = [];
 	$scope.tableUser = starTable.create($scope, 'users', false, 100);
+	$scope.tableMessage = starTable.create($scope, 'selectedMessages', false, 100);
 
 	var socket = io.connect();
 
@@ -17,6 +20,15 @@ starApp.controller('chatController', function($scope, starTable, accountService)
 
 	socket.on('message', function(data) {
 		console.log(data);
+		if (!$scope.messages[data.from]) {
+			$scope.messages[data.from] = [];
+		}
+		$scope.messages[data.from].push({
+			author: data.from,
+			message: data.message,
+			date: data.date
+		});
+		$scope.tableMessage.reload();
 	});
 
 	$scope.changeUserSelected = function(model) {
@@ -25,6 +37,11 @@ starApp.controller('chatController', function($scope, starTable, accountService)
 		});
 		model.$selected = !model.$selected;
 		$scope.user = model;
+		if (!$scope.messages[model.nickname]) {
+			$scope.messages[model.nickname] = [];
+		}
+		$scope.selectedMessages = $scope.messages[model.nickname];
+		$scope.tableMessage.reload();
 	};
 
 	$scope.writting = function() {};
@@ -33,8 +50,19 @@ starApp.controller('chatController', function($scope, starTable, accountService)
 		var data = {
 			from: accountService.getUserName(),
 			message: $scope.message,
-			to: $scope.user.nickname
+			to: $scope.user.nickname,
+			date: new Date()
 		};
+		if (!$scope.messages[data.to]) {
+			$scope.messages[data.to] = [];
+		}
+		$scope.messages[data.to].push({
+			author: data.from,
+			message: data.message,
+			date: data.date
+		});
+		$scope.tableMessage.reload();
 		socket.emit('message', data);
+		$scope.message = '';
 	};
 });
