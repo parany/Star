69e0266b5ee2295cf57817734978a946038a71da
RAMILISON Andrianandraina Andriamparany <ramilison.andriamparany@gmail.com>
2016-04-02 13:12:37 +0400

    [user] articles, activities noteService => OK

--- public/scripts/controllers/note/listNoteController.js
+++ public/scripts/controllers/note/listNoteController.js
@@ -5,7 +5,7 @@
     $scope.tableNote = starTable.create($scope, 'notes');
 
     $scope.search = function() {
-        noteService.search(accountService.getUserName(), $scope.txtSearch).success(function(data) {
+        noteService.search($scope.txtSearch).success(function(data) {
             $scope.notes = data;
             $scope.tableNote.settings().total = $scope.notes.length;
             $scope.tableNote.parameters().page = 1;
@@ -21,7 +21,7 @@
         });
         model.$selected = !model.$selected;
         $scope.page.title = 'Note - ' + model.Verse;
-        noteService.getNotes(accountService.getUserName(), model.VerseId).success(function(data) {
+        noteService.getNotes(model.VerseId).success(function(data) {
             $scope.dtoNote = data;
         });
     };
