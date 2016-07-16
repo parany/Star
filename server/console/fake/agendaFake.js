4f8a3dd9477918fc8ac612941abb1b2b56fe1dcb
ANDRIANANDRAINA Andriamparany <ramilison.andriamparany@gmail.com>
2016-07-16 16:09:00 +0400

    Update fake to allow add more fake contents.

--- server/console/fake/agendaFake.js
+++ server/console/fake/agendaFake.js
@@ -1,24 +1,24 @@
 var phony = require('phony').make_phony();
 var _ = require('underscore');
-var Repository = require('../../model/repository.js');
+var repository = require('../../model/repository.js');
 var helper = require('./helper.js');
 
-var agendasRepository = new Repository('agendas');
-
 var days = _.sample(_.range(0, 30), _.random(15, 25));
-var months = _.range(0, 4);
-var year = 2015;
+var months = _.range(6, 8);
+var year = 2016;
 var agendas = [];
 
 months.forEach(function(month) {
 	days.forEach(function(day) {
 		var nbOfItemsInDay = _.range(0, _.random(1, 4));
 		nbOfItemsInDay.forEach(function() {
+			var date = helper.getRandomTime(year, month, day);
+			console.log((new Date(date)).toLocaleString());
 			var agenda = {
 				CreatedBy: 'parany',
 				CreatedOn: new Date().getTime(),
 				UpdatedBy: 'parany',
-				Date: helper.getRandomTime(year, month, day),
+				Date: date,
 				Title: phony.lorem_title(),
 				Text: helper.getText()
 			};
@@ -27,4 +27,4 @@ months.forEach(function(month) {
 	});
 });
 
-agendasRepository.save(agendas);
+repository.insert('agendas', agendas);
--- server/console/fake/explicationFake.js
+++ server/console/fake/explicationFake.js
@@ -2,33 +2,28 @@
 
 var phony = require('phony').make_phony();
 var _ = require('underscore');
-var Repository = require('../../model/repository.js');
 var helper = require('./helper.js');
-
-var explicationsRepository = new Repository('explications');
-var tagsRepository = new Repository('tags');
-var booksRepository = new Repository('books');
-var versesRepository = new Repository('verses');
+var repository = require('../../model/repository.js');
 
 var days = _.sample(_.range(0, 30), _.random(15, 25));
-var months = _.range(0, 4);
-var year = 2015;
+var months = _.range(0, 8);
+var year = 2016;
 var allVerses = [];
 var explications = [];
 var allTags = [];
 var allBooks = [];
 var versesWithBooks = [];
 
-tagsRepository.find({
+repository.find('tags', {
 	Type: 'Explication'
 }).then(function(docTags) {
 	allTags = docTags.map(function(t) {
 		return t._id.toString();
 	});
-	return booksRepository.find({});
+	return repository.find('books', {});
 }).then(function(docBooks) {
 	allBooks = docBooks;
-	return versesRepository.find({
+	return repository.find('verses', {
 		limit: 62193
 	});
 }).then(function(docVerseIds) {
@@ -62,13 +57,14 @@ tagsRepository.find({
 						ParagraphMax: v.Paragraph + 3
 					};
 				});
-
+				var date = helper.getRandomTime(year, month, day);
+				console.log((new Date(date)).toLocaleString());
 				var explication = {
 					CreatedBy: 'parany',
 					CreatedOn: new Date().getTime(),
 					UpdatedBy: 'parany',
 					UpdatedOn: null,
-					Date: helper.getRandomTime(year, month, day),
+					Date: date,
 					Title: phony.lorem_title(),
 					Content: helper.getText(),
 					VerseReadList: versesWithBooks,
@@ -78,5 +74,5 @@ tagsRepository.find({
 			});
 		});
 	});
-	explicationsRepository.save(explications);
+	repository.insert('explications', explications);
 });
