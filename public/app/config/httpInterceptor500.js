e2bda98e9228aa2f8ab5b126f43d4f9e24aeba44
ANDRIANANDRAINA Andriamparany <ramilison.andriamparany@gmail.com>
2016-08-13 18:29:04 +0400

    -fix issue: when token is expired, header is always displayed:

--- public/app/config/httpInterceptor500.js
+++ public/app/config/httpInterceptor500.js
@@ -3,6 +3,7 @@ angular.module('starApp').factory('htppInterceptor500', ['$q', '$location', func
 		'responseError': function(rejection) {
 			if (rejection.status === 403) {
 				localStorage.removeItem('user');
+				localStorage.removeItem('token');
 				$location.path('/login');
 			} else if (rejection.status !== 401) {
 				$location.path('/error500');
