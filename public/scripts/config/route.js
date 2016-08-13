0671cb7601ef512e0f069e924fe5f3531e1b1a6b
ANDRIANANDRAINA Andriamparany <ramilison.andriamparany@gmail.com>
2016-08-13 16:30:05 +0400

    adapt folder to the best practises, ie not by file type as actually but by functionality. So, add a folder for each page.

--- public/scripts/config/route.js
+++ public/scripts/config/route.js
@@ -7,139 +7,139 @@ angular.module('starApp').config(['$routeProvider', 'ACCESS_LEVELS', function($r
             menuId: 'read'
         })
         .when('/', {
-            templateUrl: 'views/pages/read/index.html',
+            templateUrl: 'scripts/pages/read/index.html',
             controller: 'readController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'read'
         })
         .when('/notes', {
-            templateUrl: 'views/pages/note/list.html',
+            templateUrl: 'scripts/pages/note/list.html',
             controller: 'listNoteController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'read'
         })
         .when('/note/add/:id', {
-            templateUrl: 'views/pages/note/add.html',
+            templateUrl: 'scripts/pages/note/add.html',
             controller: 'addNoteController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'read'
         })
         .when('/note/edit/:id', {
-            templateUrl: 'views/pages/note/edit.html',
+            templateUrl: 'scripts/pages/note/edit.html',
             controller: 'editNoteController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'read'
         })
         .when('/agendas', {
-            templateUrl: 'views/pages/agenda/index.html',
+            templateUrl: 'scripts/pages/agenda/index.html',
             controller: 'agendaController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'agenda'
         })
         .when('/agendas/detail/:id', {
-            templateUrl: 'views/pages/agenda/detail.html',
+            templateUrl: 'scripts/pages/agenda/detail.html',
             controller: 'detailAgendaController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'agenda'
         })
         .when('/agendas/add', {
-            templateUrl: 'views/pages/agenda/add.html',
+            templateUrl: 'scripts/pages/agenda/add.html',
             controller: 'addAgendaController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'agenda'
         })
         .when('/agendas/edit/:id', {
-            templateUrl: 'views/pages/agenda/edit.html',
+            templateUrl: 'scripts/pages/agenda/edit.html',
             controller: 'editAgendaController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'agenda'
         })
         .when('/dicos', {
-            templateUrl: 'views/pages/dico/index.html',
+            templateUrl: 'scripts/pages/dico/index.html',
             controller: 'dicosController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'dico'
         })
         .when('/dicos/add', {
-            templateUrl: 'views/pages/dico/add.html',
+            templateUrl: 'scripts/pages/dico/add.html',
             controller: 'addDicoController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'dico'
         })
         .when('/dicos/edit/:id', {
-            templateUrl: 'views/pages/dico/add.html',
+            templateUrl: 'scripts/pages/dico/add.html',
             controller: 'addDicoController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'dico'
         })
         .when('/treaties', {
-            templateUrl: 'views/pages/treaty/index.html',
+            templateUrl: 'scripts/pages/treaty/index.html',
             controller: 'treatyController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'treaty'
         })
         .when('/treaties/detail/:id', {
-            templateUrl: 'views/pages/treaty/detail.html',
+            templateUrl: 'scripts/pages/treaty/detail.html',
             controller: 'detailTreatyController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'treaty'
         })
         .when('/treaties/add', {
-            templateUrl: 'views/pages/treaty/add.html',
+            templateUrl: 'scripts/pages/treaty/add.html',
             controller: 'addTreatyController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'treaty'
         })
         .when('/treaties/edit/:id', {
-            templateUrl: 'views/pages/treaty/add.html',
+            templateUrl: 'scripts/pages/treaty/add.html',
             controller: 'addTreatyController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'treaty'
         })
         .when('/explications', {
-            templateUrl: 'views/pages/explication/index.html',
+            templateUrl: 'scripts/pages/explication/index.html',
             controller: 'explicationController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'explication'
         })
         .when('/explications/add', {
-            templateUrl: 'views/pages/explication/Add.html',
+            templateUrl: 'scripts/pages/explication/Add.html',
             controller: 'addExplicationController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'explication'
         })
         .when('/explications/edit/:id', {
-            templateUrl: 'views/pages/explication/add.html',
+            templateUrl: 'scripts/pages/explication/add.html',
             controller: 'addExplicationController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'explication'
         })
         .when('/explications/detail/:id', {
-            templateUrl: 'views/pages/explication/detail.html',
+            templateUrl: 'scripts/pages/explication/detail.html',
             controller: 'detailExplicationController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'explication'
         })
         .when('/news', {
-            templateUrl: 'views/pages/new/index.html',
+            templateUrl: 'scripts/pages/new/index.html',
             controller: 'newController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'new'
         })
         .when('/news/detail/:id', {
-            templateUrl: 'views/pages/new/detail.html',
+            templateUrl: 'scripts/pages/new/detail.html',
             controller: 'detailNewController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'new'
         })
         .when('/news/add', {
-            templateUrl: 'views/pages/new/add.html',
+            templateUrl: 'scripts/pages/new/add.html',
             controller: 'addNewController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'new'
         })
         .when('/news/edit/:id', {
-            templateUrl: 'views/pages/new/add.html',
+            templateUrl: 'scripts/pages/new/add.html',
             controller: 'addNewController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'new'
@@ -151,30 +151,30 @@ angular.module('starApp').config(['$routeProvider', 'ACCESS_LEVELS', function($r
             menuId: 'account'
         })
         .when('/tweets', {
-            templateUrl: 'views/pages/tweet/index.html',
+            templateUrl: 'scripts/pages/tweet/index.html',
             controller: 'tweetController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'tweet'
         })
         .when('/tweets/add', {
-            templateUrl: 'views/pages/tweet/add.html',
+            templateUrl: 'scripts/pages/tweet/add.html',
             controller: 'addTweetController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'tweet'
         })
         .when('/tweets/edit/:id', {
-            templateUrl: 'views/pages/tweet/add.html',
+            templateUrl: 'scripts/pages/tweet/add.html',
             controller: 'addTweetController',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'tweet'
         })
         .when('/chat', {
-            templateUrl: 'views/pages/chat/index.html',
+            templateUrl: 'scripts/pages/chat/index.html',
             controller: 'chatController',
             access_level: ACCESS_LEVELS.Pub
         })
         .when('/error500', {
-            templateUrl: 'views/pages/error/500.html',
+            templateUrl: 'scripts/pages/error/500.html',
             controller: 'error500Controller',
             access_level: ACCESS_LEVELS.Pub,
             menuId: 'error'
