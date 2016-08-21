3fd17cdefb9cd349cbc93e032b71665715212e26
ANDRIANANDRAINA Andriamparany <ramilison.andriamparany@gmail.com>
2016-08-21 20:15:59 +0400

    Fix circular dependency

--- public/app/config/httpInterceptor500.js
+++ public/app/config/httpInterceptor500.js
@@ -1,10 +1,11 @@
 angular.module('starApp').factory('htppInterceptor500',
-		['$rootScope', '$q', '$location', 'accountService',
-			function($rootScope, $q, $location, accountService) {
+		['$rootScope', '$q', '$location',
+			function($rootScope, $q, $location) {
 	var interceptor = {
 		'responseError': function(rejection) {
 			if (rejection.status === 403 || rejection.status === 401) {
-				accountService.logout();
+				localStorage.removeItem('user');
+				localStorage.removeItem('token');
 				$rootScope.$emit('account.expired', true);
 			} else if (rejection.status !== 500) {
 				$location.path('/login');
